b6c4e03b2343f8665395ea5e3dcadf1b1a704273
Shresthaoshen <shresthao@mymail.vcu.edu>
2021-03-05 18:10:23 -0500

    Numerical input

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { Button, View, Image, StyleSheet, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import NumericInput from 'react-native-numeric-input'
+
+
 
 class App extends React.Component{
   state ={
@@ -15,12 +18,44 @@ function HomePage({ navigation }) {
         style={styles.tinyLogo}
         source={require('../assets/Holes.png')}
       />
+      <View style = {styles.spacing}>
       <Button
-      
+        
         title="Go to Score Card"
         onPress={() => navigation.navigate('scoreCard')}
         color = "#37D67A"
+        
       />
+
+    </View>
+    <View style = {styles.spacing}>
+
+      <Button
+        title="Add Players"
+        color = "#37D67A"
+      />
+      <NumericInput type='up-down'
+       onChange={value => console.log(value)}
+      valueType='real'
+       rounded 
+       />
+
+    </View>
+
+    <View style = {styles.spacing}>
+      <Button
+        title="Add Holes"
+        color = "#37D67A"
+      />
+
+      <NumericInput type='up-down'
+       onChange={value => console.log(value)}
+       valueType='real'
+       rounded 
+       onLimitReached={(isMax,msg) => console.log(isMax,msg)}
+       />
+
+    </View>
     </View>
   );
 }
@@ -28,6 +63,19 @@ function HomePage({ navigation }) {
 export default HomePage
 
 const styles = StyleSheet.create({
+
+  spacing: {
+    padding :15, 
+    marginRight: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignContent : 'stretch'
+
+
+  },
+
   tinyLogo: {
     marginTop:10,
     marginBottom: 20,
