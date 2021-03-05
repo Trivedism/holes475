046fe4932a7389aec73ceae2fa30507595051e78
Shresthaoshen <shresthao@mymail.vcu.edu>
2021-03-05 14:01:23 -0500

    HomeScreen Button

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -2,21 +2,58 @@ import * as React from 'react';
 import { Button, View, Image, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import NumericInput from 'react-native-numeric-input'
 
 function HomePage({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{  flexDirection: 'column',  flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Image
         style={styles.tinyLogo}
         source={require('../assets/Holes.png')}
       />
-      <Button
-        title="Go to Score Card"
-        onPress={() => navigation.navigate('scoreCard')}
-      />
+      <View style = {{padding: 10}}>
+          <Button
+             title="Go to Score Card"
+              onPress={() => navigation.navigate('scoreCard')}
+           />
+
+      </View>
+
+        <View style = {styles.spacing}>
+          <Button
+             title="Add Players"
+          />
+          <View styte = {{marginLeft: 20}}>
+          <NumericInput
+           onChange={value => console.log(value)} 
+           onLimitReached={(isMax,msg) => console.log(isMax,msg)}
+           totalWidth={70} 
+           totalHeight={30} 
+           valueType='real'
+           rounded 
+          />
+          </View>
+      </View>
+
+      <View style = {styles.spacing}>
+          <Button
+             title="Add Holes"
+          />
+          <View styte = {styles.spacing}>
+          <NumericInput
+           onChange={value => console.log(value)} 
+           onLimitReached={(isMax,msg) => console.log(isMax,msg)}
+           totalWidth={70} 
+           totalHeight={30} 
+           valueType='real'
+           rounded 
+          />
+          </View>
+      </View>
     </View>
   );
 }
+
 
 export default HomePage
 
@@ -26,7 +63,13 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     alignItems:'center',
     justifyContent:'center',
-    width: 250,
-    height: 250,
+    width: 200,
+    height: 200,
   },
+
+ spacing: {
+  flexDirection: 'row',
+  padding: 10
+ }
+
 })
