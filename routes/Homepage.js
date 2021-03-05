8041316d3c6cd7b876f9762a230e75eb38dd1007
lankalapalljn <lankalapalljn@vcu.edu>
2021-03-05 16:45:52 -0500

    changes

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -1,8 +1,13 @@
 import * as React from 'react';
-import { Button, View, Image, StyleSheet } from 'react-native';
+import { Button, View, Image, StyleSheet, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+class App extends React.Component{
+  state ={
+    color: 'Purple'
+  }
+}
 function HomePage({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -11,6 +16,7 @@ function HomePage({ navigation }) {
         source={require('../assets/Holes.png')}
       />
       <Button
+      
         title="Go to Score Card"
         onPress={() => navigation.navigate('scoreCard')}
       />
