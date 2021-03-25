056099986cef2c472f0e8f06ad76bdbcef334d7e
Shresthaoshen <shresthao@mymail.vcu.edu>
2021-03-25 17:31:22 -0400

    Fixed Allignement in Homepage

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Image, StyleSheet, TextInput } from 'react-native';
+import {ImageBackground, Button, View, Image, StyleSheet, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import NumericInput from 'react-native-numeric-input'
@@ -19,28 +19,46 @@ function HomePage({ navigation: {navigate} }) {
   const [holes, setHoles] = useState(0);
 
   return (
+    
+
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+      <View style = {{alignItems: 'center'}}>
       <Image
         style={styles.tinyLogo}
         source={require('../assets/Holes.png')}
       />
+      </View>
+
+
       <View style = {styles.spacing}>
-      <Button
-        
-        title="Go to Score Card"
+
+    <View style = {{marginHorizontal: 10, width: "68%"}}>
+     
+    
+      <Button 
+       
+        title= "Go to Score Card"
         onPress={() => navigate('scoreCard', { players: players, holes: holes })}
         color = "#37D67A"
         
       />
+      </View>
 
     </View>
+    
 
     <View style = {styles.spacing}>
 
+<View style = {{marginHorizontal: 5, width: "37%"}}>
       <Button
         title="Add Players"
         color = "#37D67A"
       />
+
+</View>
+
+<View style = {{marginLeft: 1}}>
       <NumericInput type='up-down'
        initValue= {players}
        value={players}
@@ -50,14 +68,20 @@ function HomePage({ navigation: {navigate} }) {
        maxValue={15}
        minValue={1}
        />
+ </View>
 
     </View>
 
     <View style = {styles.spacing}>
-      <Button
+  
+    <View style = {{marginHorizontal: 5, width: "37%"}}>
+      <Button 
         title="Add Holes"
         color = "#37D67A"
       />
+      </View>
+
+      <View style = {{marginLeft: 1}}>
 
       <NumericInput type='up-down'
       initValue= {holes}
@@ -71,10 +95,13 @@ function HomePage({ navigation: {navigate} }) {
        />
 
        {console.log(holes)}
+      </View>
 
     </View>
     </View>
+    
   );
+  
 }
 
 export default HomePage
@@ -83,14 +110,19 @@ const styles = StyleSheet.create({
 
   spacing: {
     padding :15, 
-    marginRight: 10,
-    marginLeft: 10,
-    marginRight: 10,
+   // marginRight: 10,
+   // marginLeft: 10,
+   // marginRight: 10,
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignContent : 'stretch'
+    //justifyContent: 'space-around',
+   // alignContent : 'stretch'
 
 
+  },
+
+  back: {
+    flex: 1,
+    flexDirection: "column"
   },
 
   tinyLogo: {
